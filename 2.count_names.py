4d1c5b01a77540b518247953c5e098fd465ec442
van81 <van81@inbox.ru>
2016-08-15 17:41:33 +0300

    Update 2.count_names.py

--- 2.count_names.py
+++ 2.count_names.py
@@ -1,13 +1,13 @@
+from collections import defaultdict
+
 def count_names():
     with open('names.txt', 'r') as f:
         data = f.readlines()
-    info = {}
+    info = defaultdict(int)
     for x in data:
       a = x.strip()
-      if not a in info:
-        info[a] = 1
-      else:
-        info[a] += 1
-    print ''.join(map(lambda x:'{0}: {1}\n'.format(x[0],x[1]),info.items()))
+      info[a] += 1
+    sinfo = sorted(info.items(),key=lambda x:x[0])
+    print ''.join(('{0}: {1}\n'.format(x[0],x[1]) for x in sinfo))
         
 count_names()
